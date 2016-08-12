fe16e04ae766e432fa4a564d6ee30eb93e6068eb
Alexey Nichiporchik <lexarx@gmail.com>
2016-08-12 16:04:20 +0100

    Converted to CommonJS module format.

--- interface.js
+++ interface.js
@@ -17,86 +17,87 @@
  * ChildInterface.isImplementedBy(child) // true
  * ParentInterface.isImplementedBy(child) // true
  */
-define('interface', [
-	'class'
-], function(Class) {	
+
+var Class = require('class');
+
+/**
+ * Checks if the provided value is an object.
+ * @param {*} value
+ * @returns {Boolean}
+ */
+function isObject(value) {
+	return value !== null && (typeof value === 'object' || typeof value === 'function');
+}
+
+/**
+ * @class Interface
+ */
+var Interface = Class.extend({
 	/**
-	 * Checks if the provided value is an object.
-	 * @param {*} value
+	 * @constructor
+	 * @param {Array<Interface>} [interfaces]
+	 */
+	constructor: function(interfaces) {
+		this.interfaces = interfaces;
+	},
+
+	/**
+	 * Marks the class as implementer of the interface.
+	 * @param {Function} classConstructor
+	 */
+	addTo: function(classConstructor) {
+		var prototype = classConstructor.prototype;
+		var interfaces = prototype.__interfaces__;
+		if (prototype.hasOwnProperty('__interfaces__')) {
+			interfaces.push(this);
+		} else {
+			var newInterfaces = [this];
+			if (interfaces === undefined) {
+				interfaces = newInterfaces;
+			} else {
+				interfaces = interfaces.concat(newInterfaces);
+			}
+			prototype.__interfaces__ = interfaces;
+		}
+	},
+
+	/**
+	 * Checks if specified object implements the interface
+	 * @param {Object} object
 	 * @returns {Boolean}
 	 */
-	function isObject(value) {
-		return value !== null && (typeof value === 'object' || typeof value === 'function');
-	}
+	isImplementedBy: function(object) {
+		if (!isObject(object)) {
+			return false;
+		}
+		var interfaces = object.__interfaces__;
+		if (interfaces === undefined) {
+			return false;
+		}
+		for (var i = 0; i < interfaces.length; i++) {
+			var iface = interfaces[i];
+			if (iface === this || iface.contains(this)) {
+				return true;
+			}
+		}
+		return false;
+	},
 
 	/**
-	 * @class Interface
+	 * @param {Interface} iface
+	 * @returns {Boolean}
 	 */
-	return Class.extend({
-		/**
-		 * @constructor
-		 * @param {Array<Interface>} [interfaces]
-		 */
-		constructor: function(interfaces) {
-			this.interfaces = interfaces;
-		},
-		
-		/**
-		 * Marks the class as implementer of the interface.
-		 * @param {Function} classConstructor
-		 */
-		addTo: function(classConstructor) {
-			var prototype = classConstructor.prototype;
-			var interfaces = prototype.__interfaces__;
-			if (prototype.hasOwnProperty('__interfaces__')) {
-				interfaces.push(this);
-			} else {
-				var newInterfaces = [this];
-				if (interfaces === undefined) {
-					interfaces = newInterfaces;
-				} else {
-					interfaces = interfaces.concat(newInterfaces);
-				}
-				prototype.__interfaces__ = interfaces;
-			}
-		},
-
-	    /**
-	 	 * Checks if specified object implements the interface
- 		 * @param {Object} object
-		 * @returns {Boolean}
-		 */
-		isImplementedBy: function(object) {
-			if (!isObject(object)) {
-				return false;
-			}
-			var interfaces = object.__interfaces__;
-			if (interfaces === undefined) {
-				return false;
-			}
-			for (var i = 0; i < interfaces.length; i++) {
-				var iface = interfaces[i];
-				if (iface === this || iface.contains(this)) {
+	contains: function(iface) {
+		if (this.interfaces !== undefined) {
+			for (var i = 0; i < this.interfaces.length; i++) {
+				var localInterface = this.interfaces[i];
+				if (localInterface === iface || localInterface.contains(iface)) {
 					return true;
 				}
 			}
-			return false;
-		},
-		
-		/**
-		 * @param {Interface} iface
-		 * @returns {Boolean}
-		 */
-		contains: function(iface) {
-			if (this.interfaces !== undefined) {
-				for (var i = 0; i < this.interfaces.length; i++) {
-					var localInterface = this.interfaces[i];
-					if (localInterface === iface || localInterface.contains(iface)) {
-						return true;
-					}
-				}
-			}
-			return false;
 		}
-	});
+		return false;
+	}
 });
+
+module.exports = Interface;
